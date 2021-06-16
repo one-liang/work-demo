15fbf8091a6f20bc043bde8b4a006c6a4d4e1b4e
Liang <william810212@gmail.com>
2021-06-17 00:24:40 +0800

    update purge content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
