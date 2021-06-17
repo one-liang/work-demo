4f9c79c036cab4b3e3503baa80563a3cea692d0d
LiMin <a0952579528@gmail.com>
2021-06-17 21:49:11 +0800

    nav create

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
     <Nav />
-    <p class="text-red-500">test</p>
     <!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> -->
   </div>
